e0f9654e15b5e8882f6438a24b15198267a32235
quadnix <quadnix1@gmail.com>
2014-07-16 10:52:53 -0400

    udated and new files

---
+++ Asteriods/Asteriods/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  Asteriods
+//
+
+//  Copyright (c) 2014 iDTech. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ PostingToTwitter/PostingToTwitter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PostingToTwitter
+//
+//  Created by iD Student on 7/16/14.
+//  Copyright (c) 2014 iDTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
